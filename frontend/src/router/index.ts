9487faec5aeab299a0c7427c3310433f9c76644b
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-04-27 23:49:41 +0700

    add layout and remove default vue stuff

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "ACCI | Home",
       component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
   ],
-})
+});
 
-export default router
+// Change the page title on navigation.
+router.beforeEach((to, from, next) => {
+  document.title = to.name?.toString() || "ACCI | Undefined";
+  next();
+});
+
+export default router;
