e2549b3ace8e0d2e37ed000a66edae89b9ff81c4
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-04-28 07:34:51 +0700

    login example layout

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,13 @@
+import LoginView from "@/views/LoginView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "ACCI | Home",
-      component: HomeView,
+      name: "ACCI | Login",
+      component: LoginView,
     },
   ],
 });
